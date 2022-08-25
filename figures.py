826760ebc9b04af89210582e043a8e00c293f615
Gregoire Degobert Yasmine <gregoire.yd@gmail.com>
2022-08-25 15:28:19 +0200

    bhv vs wheel data

--- figures.py
+++ figures.py
@@ -67,7 +67,7 @@ ax[0].bar([k for k in range(2)], [bars[b][0] for b in ['foraging_vs_exploration'
           tick_label=['foraging\nvs exploration', 'other\n(patch related)'], align='edge')
 for i, color in enumerate(['green', 'red']):
     ax[0].bar([k for k in range(2)], [bars[b][i+1] for b in ['foraging_vs_exploration', 'other_patch_related']],
-              width=0.4, bottom=i * [bars[b][1] for b in ['foraging_vs_exploration', 'other_patch_related']],
+              width=0.4, bottom=[i*bars[b][1] for b in ['foraging_vs_exploration', 'other_patch_related']],
               align='edge', lw=0, edgecolor='black', color=color)
 ax[1].bar([k for k in range(2)], [bars[b][0] for b in ['separate_foraging', 'other_non_patch_related']], width=0.4,
           tick_label=['separate\nforaging', 'other\n(non patch related)'], align='edge')
